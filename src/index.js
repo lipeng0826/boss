0ef8ff7fcdc1e92c0a37453fa1d24a286ac775ff
李鹏 <lipeng0@aixuexi.com>
2022-08-04 15:14:21 +0800

    feat: 增加一个新子应用

--- src/index.js
+++ src/index.js
@@ -12,14 +12,20 @@ root.render(
 );
 
 registerMicroApps([
+  // {
+  //   name: 'pinyin', // 目前pinyin项目没有改造成微前端项目，能打开，但是没有注册微前端
+  //   entry: 'http://localhost/pinyin/index.html',
+  //   container: '#vue', // 挂载在那个标签上
+  //   activeRule: '/pinyin.html',
+  // },
+  // {
+  //   name: 'vue app', // vue app
+  //   entry: 'http://localhost:3002',
+  //   container: '#vue',
+  //   activeRule: '/vue.html',
+  // },
   {
-    name: 'vue app', // app name registered
-    entry: 'http://localhost:3002',
-    container: '#vue',
-    activeRule: '/vue.html',
-  },
-  {
-    name: 'react app',
+    name: 'react app', // react app
     entry: 'http://localhost:3001',
     container: '#react',
     activeRule: '/react.html',
